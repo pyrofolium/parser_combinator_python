8844dbd86df3a862d93606cff892edf8ee1da2f2
Brian <heynairb@gmail.com>
2023-07-28 15:18:20 -0700

    first

---
+++ lib.py
@@ -0,0 +1,216 @@
+from abc import ABC
+from functools import reduce
+from typing import Union, List, Dict, Optional, Tuple, Any, Callable
+
+JSON = Union[str, float, bool, List['JSON'], Dict[str, 'JSON']]
+
+
+class ParserCombinator(ABC):
+    def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+        pass
+
+    def __add__(self, other: 'ParserCombinator') -> 'ParserCombinator':
+        origin = self
+
+        class Result(ParserCombinator):
+            def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+                first = origin.parse(input_str)
+                if first is not None:
+                    first_token, rest = first
+                    second = other.parse(rest)
+                    if second is None:
+                        return None
+                    else:
+                        second_token, second_rest = second
+                        return first_token + second_token, second_rest
+                else:
+                    return None
+
+        return Result()
+
+    def __mul__(self, other: 'ParserCombinator') -> 'ParserCombinator':
+        origin = self
+
+        class Result(ParserCombinator):
+            def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+                first = origin.parse(input_str)
+                if first is not None:
+                    return first
+                else:
+                    return other.parse(input_str)
+
+        return Result()
+
+
+# attempts to parse a single letter
+
+class LetterParser(ParserCombinator):
+    def __init__(self, letter: str):
+        if len(letter) > 1:
+            raise Exception("letter must be one character or less(identity)")
+        self.letter = letter
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[str], str]]:
+        if len(input_str) == 0:
+            return None
+        if input_str[0] == self.letter:
+            return [self.letter], input_str[1:]
+        else:
+            return None
+
+
+# attempts to parse the given word
+
+class WordParser(ParserCombinator):
+    def __init__(self, word: str):
+        self.word = word
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[str], str]]:
+        word_parser = reduce(lambda acc, x: acc + x, [LetterParser(letter) for letter in self.word])
+        result = word_parser.parse(input_str)
+        if result is None:
+            return None
+        else:
+            tokens, rest = result
+            final_token = reduce(lambda acc, x: acc + x, tokens)
+            return [final_token], rest
+
+
+# Takes in a parser on construction and returns a new parser that repeatedly applies the parser
+# until an error occurs. Then returns the tokens and consumed string before the error occured.
+# If First attempt at parsing returns an error then the parser will return None
+class RepeatParser(ParserCombinator):
+    def __init__(self, other_parser: ParserCombinator):
+        self.parser = other_parser
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+        result = self.parser.parse(input_str)
+        if result is None:
+            return None
+        else:
+            tokens, rest = result
+            next_result = self.parse(rest)
+            if next_result is None:
+                return tokens, rest
+            else:
+                next_tokens, next_rests = next_result
+                return tokens + next_tokens, next_rests
+
+
+# Eliminates token from the return result, it only returns the rest of consumed string and an empty token list
+# turns a regular parser into a parser such that no token is returned but the string is consumed.
+class IgnoreParser(ParserCombinator):
+    def __init__(self, other_parser: ParserCombinator):
+        self.parser = other_parser
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+        result = self.parser.parse(input_str)
+        if result is None:
+            return None
+        else:
+            tokens, rest = result
+            return [], rest
+
+
+# takes the results of another parser and attempts to convert the tokens returned into another token.
+# you supply into a constructor a function that takes a list of tokens and converts those tokens into a new token.
+class ConvertToType(ParserCombinator):
+    def __init__(self, other_parser: ParserCombinator, conversion: Callable[[List[Any]], Any]):
+        self.converter = conversion
+        self.parser = other_parser
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+        result = self.parser.parse(input_str)
+        if result is None:
+            return None
+        else:
+            tokens, rest = result
+            tokens = [self.converter(tokens)]
+            return tokens, rest
+
+
+# optional parser will take a parser and transform it into a parser that does what the original parser does
+# with the change that if the original parser returns a parse error (None) this parser will just return
+# empty tokens and the original input_str
+# ex: OptionalParser(LetterParser('A')).parse("B")
+# letter parser results in an error because it can't parse B into A so you get None
+# but optional parser turns that error into ([], "B")
+class OptionalParser(ParserCombinator):
+    def __init__(self, other_parser: ParserCombinator):
+        self.parser = other_parser
+
+    def parse(self, input_str: str) -> Optional[Tuple[List[Any], str]]:
+        result = self.parser.parse(input_str)
+        if result is None:
+            return [], input_str
+        else:
+            return result
+
+
+# LazyParser creates a parser that defers parser creation to parse time,
+# initialize it by creating your parser expression and placing it in a function,
+# then load it into the LazyParser constructor as a parameter (see example below)
+# this defers creation of the parser until it comes time to actually Parse the values
+# this is useful for parsing recursive types.
+# for example:
+# parserA = parserB + parserC
+# parserC = parserM + parserA
+# above will result in parserC undefined error.
+# parserA = lambda :parserB + parserC
+# parserC = parserM + parserA()
+# attempting to fix the above by placing parserA in a lambda to defer the definition results in max recursion error
+# fix:
+# parserA = LazyParser(lambda: parserB + parserC)
+# parserC = parserM + parserA
+#
+class LazyParser(ParserCombinator):
+    def __init__(self, other_parser_function: Callable[[], ParserCombinator]):
+        self.parser_creator = other_parser_function
+
+    def parse(self, input_str: str):
+        parser = self.parser_creator()
+        return parser.parse(input_str)
+
+
+def tokens_to_dict(input_tokens: List[JSON]) -> Dict[str, JSON]:
+    if len(input_tokens) % 2 != 0:
+        raise Exception("Must have an even number of tokens to convert to Dict")
+    i = 0
+    acc = {}
+    while i < len(input_tokens):
+        key = input_tokens[i]
+        if not isinstance(key, str):
+            raise Exception("every even index of token must be a string")
+        value = input_tokens[i + 1]
+        acc[key] = value
+        i += 2
+    return acc
+
+
+def string_to_bool(input_tokens: List[str]) -> bool:
+    if len(input_tokens) != 1:
+        raise Exception("This conversion function is designed to convert one token")
+    input_str = input_tokens[0]
+    if input_str == "true":
+        return True
+    elif input_str == "false":
+        return False
+    else:
+        raise Exception(f"Invalid bool conversion cannot convert {input_str} to boolean")
+
+
+def serialize_string_in_string(input_tokens: List[str]) -> str:
+    if input_tokens[0] == input_tokens[-1] and input_tokens[0] == '"':
+        return "".join(input_tokens[1:-1])
+    else:
+        raise Exception(f"cannot turn {input_tokens} into string, must contain quotes in the string itself")
+
+
+def null_to_none(input_tokens: List[str]) -> None:
+    if len(input_tokens) != 1:
+        raise Exception("this function can only convert 1 token")
+    input_str = input_tokens[0]
+    if input_str == "null":
+        return None
+    else:
+        raise Exception(f"Cannot convert {input_str} to None")
---
+++ main.py
@@ -0,0 +1,60 @@
+from functools import reduce
+
+from lib import LetterParser, RepeatParser, IgnoreParser, ConvertToType, \
+    OptionalParser, LazyParser, tokens_to_dict, string_to_bool, serialize_string_in_string, null_to_none
+
+WordParser = lambda word: ConvertToType(reduce(lambda acc, x: acc + x, [LetterParser(letter) for letter in word]),
+                                        lambda tokens: reduce(lambda acc, x: acc + x, tokens))
+numbers = "1234567890"
+a_bunch_of_number_parsers = [LetterParser(i) for i in numbers]
+any_number_parser = reduce(lambda acc, x: acc * x, a_bunch_of_number_parsers)
+whole_number_parser = RepeatParser(any_number_parser)
+dot_parser = OptionalParser(LetterParser("."))
+space_parser = OptionalParser(IgnoreParser(RepeatParser(LetterParser(" "))))
+float_parser = ConvertToType(
+    space_parser + whole_number_parser + dot_parser + OptionalParser(whole_number_parser) + space_parser,
+    lambda x: float("".join(x)))
+bool_parser = ConvertToType(space_parser + (WordParser("true") * WordParser("false")) + space_parser, string_to_bool)
+letters = "abcdefghijklmnopqrstuvwxyz"
+upper_case = letters.upper()
+symbols = "!@#$%^&*()-+[]{};'<>,./? "
+string_content_letter_parsers = [LetterParser(i) for i in letters + upper_case + symbols]
+string_content_parser = WordParser('\\"') * reduce(lambda acc, x: x * acc, string_content_letter_parsers)
+quote_parser = LetterParser('"')
+string_parser = ConvertToType(
+    space_parser + quote_parser + OptionalParser(RepeatParser(string_content_parser)) + quote_parser + space_parser,
+    serialize_string_in_string)
+null_parser = ConvertToType(space_parser + WordParser("null") + space_parser, null_to_none)
+json_parser = LazyParser(lambda: space_parser + (
+        float_parser * bool_parser * string_parser * array_parser * object_parser * null_parser) + space_parser)
+array_element_parser = OptionalParser(RepeatParser(json_parser + IgnoreParser(LetterParser(",")))) + OptionalParser(
+    json_parser)
+array_parser = ConvertToType(space_parser + IgnoreParser(
+    LetterParser("[")) + space_parser + array_element_parser + space_parser + IgnoreParser(
+    LetterParser("]")) + space_parser, lambda x: x)
+object_element_parser = string_parser + space_parser + IgnoreParser(
+    LetterParser(':')) + space_parser + json_parser + space_parser
+object_element_parser_with_comma = object_element_parser + IgnoreParser(LetterParser(",")) + space_parser
+full_object_element_parser = (RepeatParser(
+    object_element_parser_with_comma) + object_element_parser) * object_element_parser
+object_parser = ConvertToType(space_parser + IgnoreParser(
+    LetterParser('{')) + space_parser + full_object_element_parser + space_parser + IgnoreParser(
+    LetterParser("}")) + space_parser, tokens_to_dict)
+
+print(bool_parser.parse("true"))
+print(bool_parser.parse("    false    "))
+print(float_parser.parse("10123.1  asdsdas "))
+print(null_parser.parse("     null     "))
+print(string_parser.parse('     "hello world"'))
+print(string_parser.parse('   ""    '))
+print(array_parser.parse('    [ true, 21.222, false,     "hello world     "    ]      '))
+print(array_parser.parse('    [1]      '))
+print(array_parser.parse('    [    ]      '))
+print(object_parser.parse(
+    '   { "hello"   :      1, "world"   :2, "boom": [1,2,3, {"bam": "wam"}]    }   '))
+print(json_parser.parse(
+    '{"hello": [[[[{"hello": "world", "test": null, "more": 1.2334234, "bam": [[["hello"], 1]]}]]]]}'))
+print(json_parser.parse('null'))
+print(json_parser.parse('true'))
+print(json_parser.parse('1.2334544'))
+print(json_parser.parse('[1,2,3,4,5, {"whateve": true}]'))
