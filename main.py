f27c77ff24930046de6fce1a55f9ea6bafc7c301
Brian <heynairb@gmail.com>
2023-07-29 10:52:36 -0700

    change comments

--- main.py
+++ main.py
@@ -22,19 +22,30 @@ numbers = "1234567890"
 zero_parser = LetterParser("0")
 # multiple zeros not allowed in json spec for ints and for floats you can't have multiple zeros on the left side of
 # the "."
-# so 0000.493 is illegal, 000000 is illegal and 0. is illegal, but 0.0 is legal and 0 is legal. 0. is not legal
+# so 0000.493 is illegal, 000000 is illegal and 0. is illegal, but 0.0 is legal, 0 is legal.
 multiple_zero_parser = zero_parser + RepeatParser(zero_parser)
 a_bunch_of_number_parsers = [LetterParser(i) for i in numbers]
 any_number_parser = reduce(lambda acc, x: acc * x, a_bunch_of_number_parsers)
 whole_number_parser = RepeatParser(any_number_parser)
 sign_parser = OptionalParser(LetterParser("-"))
-exponent_parser = NotParser(multiple_zero_parser) & (sign_parser + whole_number_parser)
+exponent_parser = (
+        NotParser(multiple_zero_parser)  # 0000 invalid
+        & NotParser(sign_parser + zero_parser + whole_number_parser)  # 0454.3 invalud
+        & (sign_parser + whole_number_parser)
+)
 mantissa_parser = whole_number_parser
 dot_parser = LetterParser(".")
 space_parser = OptionalParser(IgnoreParser(RepeatParser(LetterParser(" "))))
-raw_float_parser_no_spaces = sign_parser + exponent_parser + dot_parser + mantissa_parser
-float_parser_no_spaces = ConvertToType(raw_float_parser_no_spaces, lambda x: float("".join(x)))
-float_parser = ConvertToType(space_parser + raw_float_parser_no_spaces + space_parser, lambda  x: float("".join(x)))
+raw_float_parser_no_spaces = (
+        sign_parser + exponent_parser + dot_parser + mantissa_parser
+)
+float_parser_no_spaces = ConvertToType(
+    raw_float_parser_no_spaces, lambda x: float("".join(x))
+)
+float_parser = ConvertToType(
+    space_parser + raw_float_parser_no_spaces + space_parser,
+    lambda x: float("".join(x)),
+)
 int_parser = ConvertToType(
     space_parser + exponent_parser + space_parser, lambda x: int("".join(x))
 )
@@ -59,9 +70,9 @@ string_content_letter_parsers = [
     LetterParser(i) for i in letters + upper_case + symbols
 ]
 string_content_parser = (
-    WordParser('\\"')
-    * reduce(lambda acc, x: x * acc, string_content_letter_parsers)
-    * any_number_parser
+        WordParser('\\"')
+        * reduce(lambda acc, x: x * acc, string_content_letter_parsers)
+        * any_number_parser
 )
 quote_parser = LetterParser('"')
 string_parser = ConvertToType(
@@ -77,15 +88,15 @@ null_parser = ConvertToType(
 )
 json_parser = LazyParser(
     lambda: space_parser
-    + (
-        number_parser
-        * bool_parser
-        * string_parser
-        * array_parser
-        * object_parser
-        * null_parser
-    )
-    + space_parser
+            + (
+                    number_parser
+                    * bool_parser
+                    * string_parser
+                    * array_parser
+                    * object_parser
+                    * null_parser
+            )
+            + space_parser
 )
 array_element_parser = OptionalParser(
     RepeatParser(json_parser + IgnoreParser(LetterParser(",")))
@@ -101,19 +112,19 @@ array_parser = ConvertToType(
     lambda x: x,
 )
 object_element_parser = (
-    string_parser
-    + space_parser
-    + IgnoreParser(LetterParser(":"))
-    + space_parser
-    + json_parser
-    + space_parser
+        string_parser
+        + space_parser
+        + IgnoreParser(LetterParser(":"))
+        + space_parser
+        + json_parser
+        + space_parser
 )
 object_element_parser_with_comma = (
-    object_element_parser + IgnoreParser(LetterParser(",")) + space_parser
+        object_element_parser + IgnoreParser(LetterParser(",")) + space_parser
 )
 full_object_element_parser = (
-    RepeatParser(object_element_parser_with_comma) + object_element_parser
-) * object_element_parser
+                                     RepeatParser(object_element_parser_with_comma) + object_element_parser
+                             ) * object_element_parser
 object_parser = ConvertToType(
     space_parser
     + IgnoreParser(LetterParser("{"))
@@ -149,6 +160,7 @@ print(
 print(json_parser.parse("null"))
 print(json_parser.parse("true"))
 print(json_parser.parse("1.2334544"))
-print(json_parser.parse('[1,2,3,4,5, {"whatever": true}]'))
+print(json_parser.parse('[1,2,3,4,55.546E-1, {"whatever": true}]'))
 print(json_parser.parse("0000.0"))  # should return None
-print(e_notation_parser.parse("5.23e-2"))
+print(e_notation_parser.parse("-5.23e-2"))
+print(json_parser.parse("0.0"))
